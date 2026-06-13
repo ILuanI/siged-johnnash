dba05580038ea249e78e7344603c1afb4c2c79d1
joseplocal <joseplocal>
2026-06-13 18:03:25 -0500

    ruta modulo de alumnos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DocenteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Matriculas\EstudianteWebController;
 
 Route::inertia('/', 'welcome')->name('home');
 
@@ -9,6 +10,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::resource('docentes', DocenteController::class)->except(['create', 'show', 'edit']);
 });
+
+Route::get('/matriculas/estudiantes', [EstudianteWebController::class, 'index'])->name('matriculas.estudiantes.index');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/matriculas.php';
